ab27dcb4e35a4bb6f0a764cea1e79bef9cc22504
Makar <zetikspb@yandex.ru>
2026-01-30 07:34:52 +0300

    Add CORS support for frontend on Render

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors({
+  origin: ['https://event-organizer-front.onrender.com', 'http://localhost:3000'],
+  credentials: true
+}));
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
